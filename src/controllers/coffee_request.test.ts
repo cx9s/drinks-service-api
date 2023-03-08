cd1efb83654e46791cbc9237f26e546da547270f
cx9s <cx9918@gmail.com>
2023-03-08 15:23:40 +0000

    fix: file name and test description

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -1,8 +1,8 @@
 import request from "supertest";
 import { app } from "../app";
 
-describe("Test coffeelover API endpoint request", () => {
-  test("GET should return correct message", async () => {
+describe("Test coffee API endpoint request", () => {
+  test("GET /coffeelover should return correct message", async () => {
     const res = await request(app).get("/coffeelover");
     expect(res.statusCode).toEqual(200);
     expect(res.text).toEqual("I like coffee!");
