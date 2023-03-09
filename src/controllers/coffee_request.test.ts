ed6d1c9ab36a01dec6ff21a08de90e133bca7633
cx9s <cx9918@gmail.com>
2023-03-09 00:05:27 +0000

    add more tests for coffee endpoint

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -9,3 +9,23 @@ test("GET /coffee should return correct object", async () => {
     name: "Latte",
   });
 });
+
+test("GET /coffee without param should return correct object", async () => {
+  const res = await request(app).get("/coffee");
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Latte",
+  });
+});
+
+test("GET /coffee with a different param should return correct object", async () => {
+  const res = await request(app)
+    .get("/coffee")
+    .query({ coffeeName: "espresso" });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "espresso",
+  });
+});
